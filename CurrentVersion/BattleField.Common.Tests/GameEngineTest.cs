a984ac0a95910f403afeba77294e8dfb9c3c3e71
Yordan Darakchiev <iordan93@gmail.com>
2013-05-17 14:35:31 +0300

    Add unit test for Mine initialize method

---
+++ CurrentVersion/BattleField.Common.Tests/GameEngineTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BattleField.Common.Tests
+{
+    [TestClass]
+    public class GameEngineTest
+    {
+        [TestMethod]
+        public void TestGameEngine1()
+        {
+            IUserInterface userInterface = new KeyboardInterface();
+            IRenderer renderer = new ConsoleRenderer();
+            GameField gameField = new GameField(3);
+            GameEngine gameEngine = new GameEngine(userInterface, renderer, gameField);
+            gameEngine.InitializeField();
+            gameEngine.InitializeMines();
+            Assert.IsTrue(gameEngine.GetRemainingMines() > 0);
+        }
+    }
+}
